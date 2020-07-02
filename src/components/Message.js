38022eb3656c5fab4c087d4efacc0fa8e9bf17cc
Kotaro456 <kotaro03281616@gmail.com>
2020-07-02 10:42:59 +0900

    made message component basic design

--- src/components/Message.js
+++ src/components/Message.js
@@ -3,23 +3,22 @@ import React from "react";
 class Message extends React.Component {
   render() {
     return (
-      <div>
-        <div className="appName__message">
-          <p className="appName__message-text">Have you thought your time?</p>
-          <p className="appName__message-text">
+      <div className="message">
+          <p className="message__text">Have you thought your time?</p>
+          <p className="message__text">
             I show you your time without Sleeping, Working, School
           </p>
-        </div>
 
-        <ul>
-          <li>If Your life span is 80years</li>
-          <li>Sleeping : 8 Hours</li>
-          <li>Working / School : 8 Hours</li>
+        <ul className="message__conditions">
+          <li className="message_conditions-list">If Your life span is 80years</li>
+          <li className="message_conditions-list">Sleeping : 8 Hours</li>
+          <li className="message_conditions-list">Working / School : 8 Hours</li>
         </ul>
       </div>
     );
   }
 }
+
 
 
 export default Message;
