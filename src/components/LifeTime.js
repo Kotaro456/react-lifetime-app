1ac69878bd30873f909ac90b7178e0f75542d6f3
Kotaro456 <kotaro03281616@gmail.com>
2020-07-05 10:53:17 +0900

    made a detail calculate method

--- src/components/LifeTime.js
+++ src/components/LifeTime.js
@@ -20,18 +20,68 @@ class LifeTime extends React.Component {
     // state.ageを文字列から整数に変換する
     const currentAge = parseFloat(this.state.age);
     console.log(currentAge);
-    // 現在の年齢から80歳までの年数
-    const leftAge = 80 - currentAge;
-    // 睡眠時間(8時間)を引いた残りの16時間の合計
-    const leftHours = leftAge * 5840;
-    // 仕事や学校などに必要な時間10時間とする
-    const usedTime = 2450 * leftAge;
-    // 残りの時間から仕事や学校に使った時間を引く　可処分時間
-    const lifeTime = leftHours - usedTime;
-    // 数値なので文字列に変換
-    const setLifeTime = lifeTime.toString();
-    console.log(setLifeTime);
-    this.setState({ lifeTime: setLifeTime });
+
+    // 各年代の総自由時間
+    const timeTill6 = 40880;
+    const timeTill22 = 59840;
+    const timeTill65 = 145770;
+    const timeTill80 = 87600;
+
+    // 年齢が0 ~ 6歳までの時
+    if (currentAge >= 0 && currentAge <= 6) {
+      // 現在の年齢から6歳までの残りの年数
+      const LeftYear = 7 - currentAge;
+
+      //   残りの年数 ×　１年間の自由時間
+      const freeHour = LeftYear * 5840;
+
+      //   ほかの年代の各総自由時間を足す
+      const setLifeTime = freeHour + timeTill22 + timeTill65 + timeTill80;
+
+      //   確認用
+      console.log(setLifeTime);
+      this.setState({ lifeTime: setLifeTime });
+
+      //   小中高大期間
+    } else if (currentAge >= 7 && currentAge <= 22) {
+      // 小中高と大学では休日数に差があるため分ける
+      // 小中高12年分の自由時間と大学4年分の自由時間
+      const timeOfSchool = 43080;
+      const timeOfUni = 16760;
+
+      //   小中高
+      if (currentAge <= 18) {
+        const LeftYear = 19 - currentAge;
+        const freeHour = LeftYear * 3590;
+        const setLifeTime = freeHour + timeOfUni + timeTill65 + timeTill80;
+        console.log(setLifeTime);
+        this.setState({ lifeTime: setLifeTime });
+
+        // 大学
+      } else {
+        const LeftYear = 23 - currentAge;
+        const freeHour = LeftYear * 4190;
+        const setLifeTime = freeHour + timeTill65 + timeTill80;
+        console.log(setLifeTime);
+        this.setState({ lifeTime: setLifeTime });
+      }
+
+      //   社会人
+    } else if (currentAge >= 23 && currentAge <= 65) {
+      const LeftYear = 66 - currentAge;
+      const freeHour = LeftYear * 3390;
+      const setLifeTime = freeHour + timeTill80;
+      console.log(setLifeTime);
+      this.setState({ lifeTime: setLifeTime });
+
+      //   老後
+    } else if (currentAge >= 66) {
+      const LeftYear = 81 - currentAge;
+      const freeHour = LeftYear * 5840;
+      const setLifeTime = freeHour;
+      console.log(setLifeTime);
+      this.setState({ lifeTime: setLifeTime });
+    }
   }
 
   render() {
