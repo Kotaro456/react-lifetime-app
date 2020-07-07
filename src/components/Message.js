eaba80fce6d014186a62b7c74aefac7cf0aa9256
Kotaro456 <kotaro03281616@gmail.com>
2020-07-07 09:32:44 +0900

    made design of sp size

--- src/components/Message.js
+++ src/components/Message.js
@@ -10,29 +10,29 @@ class Message extends React.Component {
         </p>
 
         <ul className="message__conditions">
-          <li className="message_conditions-list">
+          <li className="message__conditions-list">
             If Your life span is 80years
           </li>
-          <li className="message_conditions-list">Sleeping : 8 Hours</li>
-          <li className="message_conditions-list">
+          <li className="message__conditions-list">Sleeping : 8 Hours</li>
+          <li className="message__conditions-list third">
             Working / School : 10 Hours (including commuter time and break time)
           </li>
-          <li className="message_conditions-list">
+          <li className="message__conditions-list">
             0 ~ 6 years old : remove only Sleeping
           </li>
-          <li className="message_conditions-list">
+          <li className="message__conditions-list">
             7 ~ 18 years old : Holidays are 140 days, SchoolDays(6h free) are
             225days
           </li>
-          <li className="message_conditions-list">
+          <li className="message__conditions-list">
             19 ~ 22 years old : Holidays are 200 days, SchoolDays(6h free) are
             165days
           </li>
-          <li className="message_conditions-list">
+          <li className="message__conditions-list">
             23 ~ 65 years old : Holidays are 120 days, WorkDays(6h free) are
             245days
           </li>
-          <li className="message_conditions-list">
+          <li className="message__conditions-list">
             66 ~ 80 years old : remove only Sleeping
           </li>
         </ul>
